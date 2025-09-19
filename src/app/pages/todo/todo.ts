f2a2b5bca192a685033a3698ed22e9843bb3695f
khang.trannguyen <nguyenkhangdev@gmail.com>
2025-09-19 11:46:38 +0700

    feat: handle data with api Http: read, create, delete

--- src/app/pages/todo/todo.ts
+++ src/app/pages/todo/todo.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment'; 
+import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [DatePipe, FormsModule],
   templateUrl: './todo.html',
-  styleUrl: './todo.css'
+  styleUrl: './todo.css',
 })
-export class Todo {
+export class Todo implements OnInit {
+  url = `${environment.apiUrl}/tasks`;
+  newTask = {
+    title: '',
+    description: '',
+    createdAt: Date.now(),
+  };
+  tasks: any[] = [];
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.getTasks();
+  }
+  getTasks() {
+    this.http.get(this.url).subscribe({
+      next: (data: any) => {
+        this.tasks = data;
+
+        console.log(this.tasks);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+  addTask() {
+    this.newTask.createdAt = Date.now();
+    this.http.post(this.url, this.newTask).subscribe({
+      next: (data: any) => {
+        this.newTask = {
+          title: '',
+          description: '',
+          createdAt: Date.now(),
+        };
+        this.getTasks();
+      },
+      error: (err) => console.log(err),
+    });
+  }
+  deleteTask(id: string) {
+    this.http.delete(`${this.url}/${id}`).subscribe({
+      next: (data: any) => {
+        console.log('deleted this task');
+        this.getTasks();
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'https://68cbc184716562cf5074e7c6.mockapi.io/api/v1',
+};
